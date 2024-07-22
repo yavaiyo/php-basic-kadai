a0a32a006d70d69b3687c01928aafcb6aa971b99
Ryo <167420060+yavaiyo@users.noreply.github.com>
2024-07-22 19:51:05 +0900

    課題提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <p>
+            <?php
+            $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+            print_r($personal_data);
+            ?>
+        </p>
+    </body>
+</html>
