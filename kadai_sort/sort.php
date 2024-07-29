07517d22b694296bc98c3cc9ccbfcabd12f8628e
yavaiyo <bsns.gh.etc@gmail.com>
2024-07-29 14:56:08 +0900

    課題提出

---
+++ kadai_sort/sort.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <p>
+        <?php
+
+        $nums = [15, 4, 18, 23, 10 ];
+
+        function sort_2way ($array, $order) {
+
+            if ($order) {
+                echo '昇順にソートします。<br>';
+                sort($array);
+                foreach ($array as $nums) {
+                    echo $nums . '<br>';
+                }
+            } else {
+                echo '降順にソートします。<br>';
+                rsort($array);
+                foreach ($array as $nums) {
+                    echo $nums . '<br>';
+                }
+            }
+        }
+
+        sort_2way($nums, TRUE);
+        sort_2way($nums, FALSE);
+        ?>
+    </p>
+</body>
+</html>
