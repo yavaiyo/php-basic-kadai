e749f3425455ea6898373093aeee58d0406c10da
yavaiyo <bsns.gh.etc@gmail.com>
2024-08-13 15:18:33 +0900

    課題提出

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h2>登録が完了しました。</h2>
+    <p>社員情報がデータベースに保存されました。</p>
+    <button id="back-btn" onclick="location.href='form.php';">戻る</button>
+</body>
+</html>
